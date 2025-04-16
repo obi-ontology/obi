93a1dba88d8547619c329012a0f162c10749203c
Sebastian Duesing <66700705+sebastianduesing@users.noreply.github.com>
2025-04-16 10:55:27 -0500

    Update import script

--- src/scripts/import.py
+++ src/scripts/import.py
@@ -35,36 +35,50 @@ def dict2TSV(xdict, path):
     rows = [i for i in xdict.keys()]
     first = rows[0]
     fieldnames = [i for i in xdict[first].keys()]
+    ids = []
+    for key in xdict.keys():
+        if key != "robot":
+            ids.append(key)
+    sorted_ids = sorted(ids)
     with open(path, "w", newline="\n", encoding='utf-8') as tsv:
         writer = csv.DictWriter(tsv, fieldnames=fieldnames, delimiter="\t")
         writer.writeheader()
-        for (id, row) in xdict.items():
-            writer.writerow(row)
+        writer.writerow(xdict["robot"])
+        for id in sorted_ids:
+            writer.writerow(xdict[id])
 
 
-def get_paths(ontology):
-    base_path = os.path.join("src", "ontology", "robot_inputs")
-    inputs = os.path.join(base_path, f"{ontology}_input.txt")
-    blocklist = os.path.join(base_path, f"{ontology}_block.txt")
-    edit = os.path.join(base_path, f"{ontology}_edit.tsv")
-    return inputs, blocklist, edit
+def read_to_list(txt):
+    output = []
+    with open(txt, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            output.append(line.strip())
+    return output
 
 
-def check_files(ontology):
-    inputs, blocklist, edit = get_paths(ontology)
-    exists = True
-    for file in inputs, blocklist, edit:
-        if not os.path.exists(file):
-            print(f"Didn't find {file}")
-            exists = False
-    return exists
-
-
-def id_to_purl(id):
-    id = id.replace(":", "_")
-    id = id.upper()
-    purl = f"http://purl.obolibrary.org/obo/{id}"
-    return purl
+def convert(string, format):
+    iri = re.search(r"http:\/\/purl\.obolibrary\.org\/obo\/([a-zA-Z]+)_(\d+)", string)
+    curie = re.search(r"([a-zA-Z]+):(\d+)", string)
+    if not iri and not curie:
+        output = None
+        print(f"{string} isn't an IRI or a CURIE")
+        quit()
+    elif format == "iri":
+        if iri:
+            output = string
+        elif curie:
+            base = curie.group(1)
+            num = curie.group(2)
+            output = f"http://purl.obolibrary.org/obo/{base}_{num}"
+    elif format == "curie":
+        if curie:
+            output = string
+        elif iri:
+            base = iri.group(1)
+            num = iri.group(2)
+            output = f"{base}:{num}"
+    return output
 
 
 def lookup_label(id):
@@ -79,118 +93,87 @@ def lookup_label(id):
     return output
 
 
-def add_label_comment(purl):
-    comment = re.search(r"#\s*[\w\d\s]", purl)
-    if not comment:
-        iri = re.search(r"([a-zA-Z]+)_(\d+)", purl)
-        if iri:
-            curie = f"{iri.group(1)}:{iri.group(2)}"
-            label = lookup_label(curie)
-            purl = re.sub("\n", f" # {label}\n", purl)
-    return purl
-
-
-def read_purls(path):
-    with open(path, "r") as text:
-        purls = text.readlines()
-        output = []
-        for purl in purls:
-            output.append(purl.strip())
-    return output
-
-
-def write_purls(purl_list, path):
-    output_purls = []
-    for purl in purl_list:
-        line_broken_purl = f"{purl}\n"
-        labeled_purl = add_label_comment(line_broken_purl)
-        if purl != "":
-            output_purls.append(labeled_purl)
-    f = open(path, "w")
-    f.writelines(output_purls)
-
-
-def add_term_to_file(term, path):
-    purls = read_purls(path)
-    purl_to_add = id_to_purl(term)
-    in_file = False
-    for purl in purls:
-        if purl_to_add in purl:
-            in_file = True
-            break
-    if in_file:
-        print(f"{term} is already in {path}")
-    else:
-        purls.append(purl_to_add)
-        purls = sorted(purls)
-        write_purls(purls, path)
-        print(f"Added {term} to {path}")
-
-
-def remove_term_from_file(term, path):
-    if ".tsv" in path:
-        template_dict = TSV2dict(path)
-        if term not in template_dict.keys():
-            print(f"Confirmed {term} is not in {path}")
+def add(term, imports):
+    term = convert(term, "curie")
+    act = True
+    if term in imports.keys():
+        act = False
+        if imports[term]["action"] != "block":
+            print(f"{term} is already in this import file")
         else:
-            del template_dict[term]
-            print(f"Removed {term} from {path}")
-    elif ".txt" in path:
-        purls = read_purls(path)
-        purl_to_remove = id_to_purl(term)
-        in_file = False
-        output_purls = []
-        for purl in purls:
-            if purl_to_remove in purl:
-                in_file = True
-            else:
-                output_purls.append(purl)
-        if not in_file:
-            print(f"Confirmed {term} is not in {path}")
-        else:
-            if len(output_purls) >= len(purls):
-                print("STOPPED: Output contains more IRIs than input")
-            else:
-                write_purls(output_purls, path)
-                print(f"Removed {term} from {path}")
-
-
-def add(term, ontology):
-    inputs, blocklist, edit = get_paths(ontology)
-    add_term_to_file(term, inputs)
-    remove_term_from_file(term, blocklist)
-
-
-def block(term, ontology):
-    inputs, blocklist, edit = get_paths(ontology)
-    add_term_to_file(term, blocklist)
-    remove_term_from_file(term, inputs)
-    remove_term_from_file(term, edit)
-
-
-def drop(term, ontology):
-    inputs, blocklist, edit = get_paths(ontology)
-    remove_term_from_file(term, inputs)
-    remove_term_from_file(term, blocklist)
-    remove_term_from_file(term, edit)
-
-
-def parent(term, parent, ontology):
-    inputs, blocklist, edit = get_paths(ontology)
-    add_term_to_file(term, inputs)
-    remove_term_from_file(term, blocklist)
-    template_dict = TSV2dict(edit)
-    parent_label = lookup_label(term)
-    if term in template_dict.keys() and template_dict[term]["parent class"] == parent_label:
-        print(f"{term} is already a subclass of '{parent}' in {edit}")
-    else:
-        template_dict[term] = {
+            override = input(f"{term} is blocked out of this import. Override? (y/n)\n")
+            if override.lower() == "y" or override.lower() == "yes":
+                act = True
+    if act:
+        label = lookup_label(term)
+        imports[term] = {
             "ontology ID": term,
-            "logical type": "subclass",
-            "parent class": parent_label,
+            "label": label,
+            "action": "input",
+            "logical type": "",
+            "parent class": ""
         }
-        dict2TSV(template_dict, edit)
-        print(f"Asserted {term} is a subclass of '{parent}'")
+        print(f"Added {term} to import")
+
+
+def block(term, imports):
+    term = convert(term, "curie")
+    if term in imports.keys() and imports[term]["action"] == "block":
+        print(f"{term} is already blocked out of this import")
+    else:
+        label = lookup_label(term)
+        imports[term] = {
+            "ontology ID": term,
+            "label": label,
+            "action": "block",
+            "logical type": "",
+            "parent class": ""
+        }
+        print(f"Blocked {term} out of this import")
+
+
+def drop(term, imports):
+    term = convert(term, "curie")
+    if term not in imports.keys():
+        print(f"{term} is not in this import file")
+    else:
+        if imports[term]["action"] == "block":
+            override = input(f"{term} is blocked out of this import. Drop the block? (y/n)\n")   
+            if override.lower() == "y" or override.lower() == "yes":
+                act = True
+            else:
+                act = False
+                print(f"{term} was not dropped from this import file")
+        else:
+            act = True
+        if act:
+            del imports[term]
+            print(f"Dropped {term} from this import file")
+
+
+def parent(term, parent, imports):
+    term = convert(term, "curie")
+    act = True
+    if term in imports.keys():
+        if imports[term]["action"] == "block":
+            act = False
+            override = input(f"{term} is blocked out of this import. Do you want to import this term? (y/n)\n")
+            if override.lower() == "y" or override.lower() == "yes":
+                act = True
+        elif imports[term]["action"] == "parent":
+            if imports[term]["parent class"] == parent:
+                act = False
+                print(f"{term} is already imported as a subclass of {parent}")
+    if act:
+        label = lookup_label(term)
+        imports[term] = {
+            "ontology ID": term,
+            "label": label,
+            "action": "parent",
+            "logical type": "subclass",
+            "parent class": parent
+        }
+        print(f"Added {term} to import as a subclass of {parent}")
 
 
 def main():
@@ -200,22 +183,59 @@ def main():
                         help="Action to take for chosen term, e.g., add")
     parser.add_argument("--ontology", "-o", type=str, required=True,
                         help="Which ontology import to edit, e.g., Uberon")
-    parser.add_argument("--term", "-t", type=str, required=True,
+    parser.add_argument("--term", "-t", type=str,
                         help="An ontology term ID, e.g., UBERON:0000465")
+    parser.add_argument("--termlist", "-l", type=str,
+                        help="A text file containing a list of ontology IDs")
     parser.add_argument("--parent", "-p", type=str,
-                        help="Intended parent for term, e.g., 'material entity'")
+                        help="Intended parent for term, e.g., 'organ'")
     args = parser.parse_args()
-    if check_files(args.ontology):
+    path = os.path.join("src", "ontology", "robot_inputs", f"{args.ontology}_input.tsv")
+    if not os.path.exists(path):
+        print(f"Didn't find {path}")
+    elif not args.term and not args.termlist:
+        print("Use --term or --termlist to indicate the term(s) to act on.")
+    else:
+        imports = TSV2dict(path)
         if args.action == "add":
-            add(args.term, args.ontology)
-            if args.parent:
-                parent(args.term, args.parent, args.ontology)
+            if args.term and args.parent:
+                add(args.term, imports)
+                parent(args.term, args.parent, imports)
+            elif args.term:
+                add(args.term, imports)
+            elif args.termlist and args.parent:
+                terms = read_to_list(args.termlist)
+                for term in terms:
+                    add(term, imports)
+                    parent(term, args.parent, imports)
+            elif args.termlist:
+                terms = read_to_list(args.termlist)
+                for term in terms:
+                    add(term, imports)
         if args.action == "block":
-            block(args.term, args.ontology)
+            if args.term:
+                block(args.term, imports)
+            elif args.termlist:
+                terms = read_to_list(args.termlist)
+                for term in terms:
+                    block(term, imports)
         if args.action == "drop":
-            drop(args.term, args.ontology)
+            if args.term:
+                drop(args.term, imports)
+            elif args.termlist:
+                terms = read_to_list(args.termlist)
+                for term in terms:
+                    drop(term, imports)
         if args.action == "parent":
-            parent(args.term, args.parent, args.ontology)
+            if not args.parent:
+                print("No parent term specified. Use --parent to set parent")
+            elif args.term:
+                parent(args.term, args.parent, imports)
+            else:
+                terms = read_to_list(args.termlist)
+                for term in terms:
+                    parent(term, args.parent, imports)
+        dict2TSV(imports, path)
 
 
 if __name__ == "__main__":
